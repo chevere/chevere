5a866ce963b0269145b54c2896e5137ab4292f9c
rodolfoberrios <inbox@rodolfoberrios.com>
2019-03-08 23:51:54 -0300

    Added "Hello, World!" to GET /

--- app/callables/index.php
+++ app/callables/index.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 namespace App;
 
 use function Chevereto\Core\dump;
+use Chevereto\Core\Json;
+use Chevereto\Core\Http\JsonResponse;
 
 return new class extends Controller {
-    public function __invoke(User $var1 = null, $var2, $var0)
+    public function __invoke()
     {
-        dump([
-            '$var0' => $var0,
-            '$var1'=> $var1,
-            '$var2'=> $var2
-        ]);
+        $json = new Json();
+        $json->setResponse('Hello, World!', 100);
+        $json
+            ->addData('api', ['entry' => 'HTTP GET /api', 'description' => 'Retrieves the exposed API.'])
+            ->addData('cli', ['entry' => 'php app/console list', 'description' => 'Lists console commands.']);
+        
+        return (new JsonResponse())->setContent($json)->setStatusCode(200);
     }
 };
-// return function (User $var1 = null, $var2, $var0) {
-//     Dumper::dump([
-//         '$var0' => $var0,
-//         '$var1'=> $var1,
-//         '$var2'=> $var2
-//     ]);
-// };
