737b19829a910115172ae9748cc0d655fc9d2bd0
rodolfoberrios <inbox@rodolfoberrios.com>
2019-03-20 16:57:49 -0300

    Fixed dump, dd

--- Chevereto-Core/src/Dumper.php
+++ Chevereto-Core/src/Dumper.php
@@ -31,6 +31,10 @@ class Dumper
             return;
         }
         $trace = debug_backtrace();
+        $callerFile = Path::normalize($trace[0]['file']);
+        if (Utils\Str::endsWith('resources/functions/dump.php', $callerFile) && $trace[0]['class'] == __CLASS__ && $trace[0]['function'] == __FUNCTION__) {
+            array_shift($trace);
+        }
         $caller = $trace[0];
         // Avoid being self dumped, use VarDumper::dump to dump here.
         while (isset($trace[0]['file']) && $trace[0]['file'] == __FILE__) {
