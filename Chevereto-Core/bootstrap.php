4535fbc31f1e3c09773fe268060d7842ee4be159
rodolfoberrios <inbox@rodolfoberrios.com>
2019-03-25 19:09:45 -0300

    Removed vars accesible using globals

--- Chevereto-Core/bootstrap.php
+++ Chevereto-Core/bootstrap.php
@@ -7,14 +7,6 @@ namespace Chevereto\Core;
 define(__NAMESPACE__.'\TIME_BOOTSTRAP', microtime(true));
 define(__NAMESPACE__.'\ERROR_LEVEL_BOOTSTRAP', error_reporting());
 
-/**
- * Assumeing that this file has been loaded from /app/bootstrap.php:.
- */
-$bootstrapper = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[0]['file'];
-$ROOT_PATH = rtrim(str_replace('\\', '/', dirname($bootstrapper, 2)), '/').'/';
-$PATH = rtrim(str_replace($ROOT_PATH, null, str_replace('\\', '/', __DIR__)), '/').'/';
-$AppPATH = basename(dirname($bootstrapper)).'/';
-
 // Namespace handles (adds trailing slashes)
 const CORE_NS_HANDLE = __NAMESPACE__.'\\';
 const APP_NS_HANDLE = 'App\\';
@@ -22,22 +14,27 @@ const APP_NS_HANDLE = 'App\\';
 const NS_HANDLE_LENGTHS = [CORE_NS_HANDLE => 15, APP_NS_HANDLE => 4];
 
 /*
+ * Assuming that this file has been loaded from /app/bootstrap.php
+ */
+define(CORE_NS_HANDLE.'BOOTSTRAPPER', debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[0]['file']);
+
+/*
  * Chevereto\Core\ROOT_PATH
  * Root path containing /app
  */
-define(CORE_NS_HANDLE.'ROOT_PATH', $ROOT_PATH);
+define(CORE_NS_HANDLE.'ROOT_PATH', rtrim(str_replace('\\', '/', dirname(BOOTSTRAPPER, 2)), '/').'/');
 
 /*
  * Chevereto\Core\PATH
  * Relative path to Chevereto\Core, usually 'vendor/chevereto/chevereto-core'
  */
-define(CORE_NS_HANDLE.'PATH', $PATH);
+define(CORE_NS_HANDLE.'PATH', rtrim(str_replace(ROOT_PATH, null, str_replace('\\', '/', __DIR__)), '/').'/');
 
 /*
  * Chevereto\Core\App\PATH
  * Relative path to app, usually 'app'
  */
-define(CORE_NS_HANDLE.'App\PATH', $AppPATH);
+define(CORE_NS_HANDLE.'App\PATH', basename(dirname(BOOTSTRAPPER)).'/');
 
 // Init console if sapi = cli
 if (php_sapi_name() == 'cli') {
@@ -74,7 +71,7 @@ App::setDefaultRuntime(
                 RuntimeConfig::TIMEZONE => 'UTC',
                 RuntimeConfig::URI_SCHEME => 'https',
             ] + DEFAULT_ERROR_HANDLING)
-            ->addFile(':config')
+            ->addFile(App::FILEHANDLE_CONFIG)
             ->process()
     )
 );
