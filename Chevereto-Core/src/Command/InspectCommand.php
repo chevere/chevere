9606b67d0e636b8f7d83f4008d2ec36eabe8ef01
rodolfoberrios <inbox@rodolfoberrios.com>
2019-03-28 04:44:30 -0300

    Inspect command now supports any callable

--- Chevereto-Core/src/Command/InspectCommand.php
+++ Chevereto-Core/src/Command/InspectCommand.php
@@ -12,13 +12,16 @@ declare(strict_types=1);
 
 namespace Chevereto\Core\Command;
 
-use ReflectionMethod;
 use Chevereto\Core\App;
+use Chevereto\Core\Load;
+use Chevereto\Core\Message;
 use Chevereto\Core\Path;
 use Chevereto\Core\Command;
 use Chevereto\Core\File;
-use Chevereto\Core\Message;
 use Chevereto\Core\Utils\Dump;
+use Chevereto\Core\Utils\Str;
+use ReflectionMethod;
+use ReflectionFunction;
 
 /**
  * The InspectCommand allows to get callable information using CLI.
@@ -45,38 +48,70 @@ class InspectCommand extends Command
     {
         $cli = $this->getCli();
         $io = $cli->getIo();
-        $callable = $cli->getInput()->getArgument('callable');
-        $callableFilePath = Path::fromHandle($callable);
-        if (File::exists($callableFilePath) == false) {
-            $io->error(
-                (string) (new Message("Callable %s doesn't exists"))
-                    ->strtr('%s', $callableFilePath)
-            );
+        $callableInput = $cli->getInput()->getArgument('callable');
 
-            return 1;
-        }
-        $controller = include $callableFilePath;
-        $invoke = new ReflectionMethod($controller, '__invoke');
-        $dir = Path::relative(dirname($callableFilePath), App::APP);
-        $resource = [];
-        do {
-            // FIXME: Usar metodo para resolver $resourceFilePath
-            $resourceFilePathRelative = $dir.'/resource.json';
-            $resourceFilePath = App\PATH.$resourceFilePathRelative;
-            if (File::exists($resourceFilePath) && $resourceString = file_get_contents($resourceFilePath)) {
-                $resourceArr = json_decode($resourceString, true)['wildcards'] ?? [];
-                $resource = array_merge($resourceArr, $resource);
+        $isCallable = is_callable($callableInput);
+        // $classExists = class_exists($callableInput);
+        if ($isCallable) {
+            $callable = $callableInput;
+            $callableSome = $callableInput;
+        } else {
+            $callableFilepath = Path::fromHandle($callableInput);
+            if (false == File::exists($callableFilepath)) {
+                $io->error(sprintf('Unable to locate callable %s', $callableInput));
+
+                return 0;
             }
-            $dir = $dir ? dirname($dir) : '.';
-        } while ($dir != '.');
-        //black, red, green, yellow, blue, magenta, cyan, white, default
-        $io->text('<fg=blue>'.$callableFilePath.'</>');
+            $callableSome = $callableFilepath;
+            $callable = Load::php($callableFilepath);
+            if (false == is_callable($callable)) {
+                $io->error(
+                    (new Message('Expecting %t return type, %s provided in %f'))
+                        ->code('%t', 'callable')
+                        ->code('%s', gettype($callable))
+                        ->code('%f', $callableInput)
+                );
+
+                return 0;
+            }
+        }
+        if (is_object($callable)) {
+            $method = '__invoke';
+        } else {
+            if (Str::contains('::', $callable)) {
+                $callableExplode = explode('::', $callable);
+                $callable = $callableExplode[0];
+                $method = $callableExplode[1];
+            }
+        }
+        if (isset($method)) {
+            $reflection = new ReflectionMethod($callable, $method);
+        } else {
+            $reflection = new ReflectionFunction($callable);
+        }
+
+        $io->block($callableSome, 'INSPECTED', 'fg=black;bg=green', ' ', true);
+
+        $resource = [];
+        if (isset($callableFilepath)) {
+            $dir = Path::relative(dirname($callableFilepath), App::APP);
+            do {
+                $resourceFilePathRelative = $dir.'/resource.json';
+                $resourceFilePath = App\PATH.$resourceFilePathRelative;
+                if (File::exists($resourceFilePath) && $resourceString = file_get_contents($resourceFilePath)) {
+                    $resourceArr = json_decode($resourceString, true)['wildcards'] ?? [];
+                    $resource = array_merge($resourceArr, $resource);
+                }
+                $dir = $dir ? dirname($dir) : '.';
+            } while ($dir != '.');
+        }
+
         $arguments = [];
-        if ($invoke->getDeclaringClass()->isInternal()) {
+        if ($reflection->getDeclaringClass()->isInternal()) {
             $io->note('Cannot determine default value for internal functions');
         }
-        $io->text([null, '<fg=yellow>Arguments:</>']);
-        foreach ($invoke->getParameters() as $parameter) {
+        $i = 0;
+        foreach ($reflection->getParameters() as $parameter) {
             $aux = null;
             if ($parameter->getType()) {
                 $aux .= $parameter->getType().' ';
@@ -85,12 +120,18 @@ class InspectCommand extends Command
             if ($parameter->isDefaultValueAvailable()) {
                 $aux .= ' = '.($parameter->getDefaultValue() ?? 'null');
             }
-            if ($resource != null && $res = $resource[$parameter->getName()] ?? null) {
-                $aux .= ' '.Dump::wrap(Dump::_OPERATOR, '--desc '.$res['description'].' --regex '.$res['regex']);
+            if (null != $resource && $res = $resource[$parameter->getName()] ?? null) {
+                $aux .= ' '.Dump::wrap(Dump::_OPERATOR, '--description '.$res['description'].' --regex '.$res['regex']);
             }
-            $arguments[] = $aux;
+            $arguments[] = "#$i $aux";
+            ++$i;
         }
-        $io->listing($arguments);
+        if (null != $arguments) {
+            $io->text(['<fg=yellow>Arguments:</>']);
+            $io->listing($arguments);
+        } else {
+            $io->text(['<fg=yellow>No arguments</>', null]);
+        }
 
         return 1;
     }
