e6c1bd1e29c843adaaa5ab849b0a37975814e299
Rodolfo Berrios <inbox@rodolfoberrios.com>
2019-05-15 12:35:56 -0400

    🐊 wip

--- Chevereto-Chevere/src/Dumper.php
+++ Chevereto-Chevere/src/Dumper.php
@@ -18,11 +18,14 @@ use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 
+/**
+ * Dumps information about one or more variables.
+ */
 class Dumper
 {
     const BACKGROUND = '#2c3e50';
     const BACKGROUND_SHADE = '#2c3e50';
-    const STYLE = 'font: 16px Consolas, monospace, sans-serif; color: #ecf0f1; padding: 15px; margin: 10px 0; word-break: break-word; white-space: pre-wrap; background: '.self::BACKGROUND.'; display: block; text-align: left; border: none; border-radius: 4px;';
+    const STYLE = 'font: 16px Consolas, monospace, sans-serif; color: #ecf0f1; padding: 15px; margin: 10px 0; word-break: break-word; white-space: pre-wrap; background: ' . self::BACKGROUND . '; display: block; text-align: left; border: none; border-radius: 4px;';
 
     protected $vars;
 
@@ -32,15 +35,19 @@ class Dumper
     /** @var ConsoleOutputInterface */
     protected $consoleOutput;
 
-    protected $string;
+    /** @var string */
+    protected $output;
 
     /** @var string */
     protected $outputHr;
 
+    /** @var array */
     protected $debugBacktrace;
 
+    /** @var string */
     protected $caller;
 
+    /** @var string */
     protected $callerFilepath;
 
     /** @var int */
@@ -50,36 +57,33 @@ class Dumper
     {
         $this->vars = $vars;
         $this->numArgs = func_num_args();
-    }
-
-    /**
-     * Dumps information about one or more variables.
-     */
-    public static function dump(...$vars): void
-    {
-        $that = new static(...$vars);
-        if ($that->numArgs == 0) {
+        if (0 == $this->numArgs) {
             return;
         }
-        $that->debugBacktrace = debug_backtrace();
-        $that->caller = $that->debugBacktrace[0];
-        $that->handleDebugBacktrace();
-        $that->setCallerFilepath($that->debugBacktrace[0]['file']);
-        $that->handleSelfCaller();
-        $that->string = null;
-        $that->handleOutput();
-        $that->handleClass();
-        $that->appendFunction($that->debugBacktrace[$that->offset]['function']);
-        $that->handleFile();
-        $that->string .= "\n\n";
-        $that->handleArgs();
-        $that->string = trim($that->string).'</pre>';
-        $that->handleProccessOutput();
+        $this->debugBacktrace = debug_backtrace();
+        $this->caller = $this->debugBacktrace[0];
+        $this->handleDebugBacktrace();
+        $this->setCallerFilepath($this->debugBacktrace[0]['file']);
+        $this->handleSelfCaller();
+        $this->output = null;
+        $this->handleOutput();
+        $this->handleClass();
+        $this->appendFunction($this->debugBacktrace[$this->offset]['function']);
+        $this->handleFile();
+        $this->output .= "\n\n";
+        $this->handleArgs();
+        $this->output = trim($this->output) . '</pre>';
+        $this->handleProccessOutput();
+    }
+
+    public static function dump(...$vars): void
+    {
+        new static(...$vars);
     }
 
     protected function handleDebugBacktrace(): void
     {
-        while (isset($this->debugBacktrace[0]['file']) && $this->debugBacktrace[0]['file'] == __FILE__) {
+        while (isset($this->debugBacktrace[0]['file']) && __FILE__ == $this->debugBacktrace[0]['file']) {
             $this->shiftDebugBacktrace();
             $this->setCaller($this->debugBacktrace[0]);
         }
@@ -97,7 +101,7 @@ class Dumper
 
     protected function handleSelfCaller(): void
     {
-        if (Utils\Str::endsWith('resources/functions/dump.php', $this->callerFilepath) && $this->debugBacktrace[0]['class'] == __CLASS__ && in_array($this->debugBacktrace[0]['function'], ['dump', 'dd'])) {
+        if (Utils\Str::endsWith('resources/functions/dump.php', $this->callerFilepath) && __CLASS__ == $this->debugBacktrace[0]['class'] && in_array($this->debugBacktrace[0]['function'], ['dump', 'dd'])) {
             $this->shiftDebugBacktrace();
         }
     }
@@ -109,7 +113,7 @@ class Dumper
 
     protected function handleOutput(): void
     {
-        if (php_sapi_name() == 'cli') {
+        if ('cli' == php_sapi_name()) {
             $this->handleConsoleOutput();
         } else {
             $this->handleHtmlOutput();
@@ -124,10 +128,10 @@ class Dumper
         $this->consoleOutput->getFormatter()->setStyle('block', new OutputFormatterStyle('red', 'black'));
         $this->consoleOutput->getFormatter()->setStyle('dumper', new OutputFormatterStyle('blue', null, ['bold']));
         $this->consoleOutput->getFormatter()->setStyle('hr', new OutputFormatterStyle('blue'));
-        $this->outputHr = '<hr>'.str_repeat('-', 60).'</>';
+        $this->outputHr = '<hr>' . str_repeat('-', 60) . '</>';
         $this->consoleOutput->getFormatter()->setStyle('hr', new OutputFormatterStyle('blue', null));
-        $maker = (isset($this->caller['class']) ? $this->caller['class'].$this->caller['type'] : null).$this->caller['function'].'()';
-        $this->consoleOutput->writeln(['', '<dumper>'.$maker.'</>', $this->outputHr]);
+        $maker = (isset($this->caller['class']) ? $this->caller['class'] . $this->caller['type'] : null) . $this->caller['function'] . '()';
+        $this->consoleOutput->writeln(['', '<dumper>' . $maker . '</>', $this->outputHr]);
     }
 
     protected function handleHtmlOutput(): void
@@ -140,12 +144,12 @@ class Dumper
 
     protected function appendHtmlOpenBody(): void
     {
-        $this->string .= '<html style="background: '.static::BACKGROUND_SHADE.';"><head></head><body>';
+        $this->output .= '<html style="background: ' . static::BACKGROUND_SHADE . ';"><head></head><body>';
     }
 
     protected function appendStyle(): void
     {
-        $this->string .= '<pre style="'.static::STYLE.'">';
+        $this->output .= '<pre style="' . static::STYLE . '">';
     }
 
     protected function handleClass(): void
@@ -161,12 +165,12 @@ class Dumper
 
     protected function appendClass(string $class, string $type): void
     {
-        $this->string .= Utils\Dump::wrap('_class', $class).$type;
+        $this->output .= Utils\Dump::wrap('_class', $class) . $type;
     }
 
     protected function appendFunction(string $function): void
     {
-        $this->string .= Utils\Dump::wrap('_function', $function.'()');
+        $this->output .= Utils\Dump::wrap('_function', $function . '()');
     }
 
     protected function handleFile(): void
@@ -178,7 +182,7 @@ class Dumper
 
     protected function appendFilepath(string $file, int $line): void
     {
-        $this->string .= "\n".Utils\Dump::wrap('_file', Path::normalize($file).':'.$line);
+        $this->output .= "\n" . Utils\Dump::wrap('_file', Path::normalize($file) . ':' . $line);
     }
 
     protected function handleArgs(): void
@@ -192,7 +196,7 @@ class Dumper
 
     protected function appendArg(int $pos, $value): void
     {
-        $this->string .= 'Arg#'.$pos.' '.Utils\Dump::out($value, 0)."\n\n";
+        $this->output .= 'Arg#' . $pos . ' ' . Utils\Dump::out($value, 0) . "\n\n";
     }
 
     protected function handleProccessOutput(): void
@@ -206,14 +210,14 @@ class Dumper
 
     protected function processConsoleOutput(): void
     {
-        $stripped = strip_tags($this->string);
+        $stripped = strip_tags($this->output);
         $this->consoleOutput->writeln($stripped, ConsoleOutput::OUTPUT_RAW);
         isset($that->outputHr) ? $this->consoleOutput->writeln($that->outputHr) : null;
     }
 
     protected function processPrintOutput(): void
     {
-        echo $this->string;
+        echo $this->output;
     }
 
     /**
