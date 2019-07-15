01b82ef785054ffcd642a40fb0df2925720de7b3
Rodolfo Berrios <inbox@rodolfoberrios.com>
2019-07-15 15:34:53 -0400

    🚧 wip

--- Chevereto-Chevere/src/Route.php
+++ Chevereto-Chevere/src/Route.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 
 namespace Chevereto\Chevere;
 
-use Throwable;
 use Exception;
 use Symfony\Component\Console\Exception\LogicException;
 
@@ -137,19 +136,13 @@ class Route
     public function setName(string $name): self
     {
         // Validate $name
-        try {
-            Validation::grouped('$name', $name)
-                ->append(
-                    'value',
-                    function (string $string): bool {
-                        return (bool) preg_match(Route::REGEX_NAME, $string);
-                    },
-                    (string) (new Message("Expecting at least one alphanumeric, underscore, hypen or dot character. String '%s' provided."))
+        if (!preg_match(Route::REGEX_NAME, $name)) {
+            throw new Exception(
+                (string)
+                    (new Message("Expecting at least one alphanumeric, underscore, hypen or dot character. String '%s' provided."))
+                        ->code('%s', $name)
                         ->code('%p', Route::REGEX_NAME)
-                )
-                ->validate();
-        } catch (Exception $e) {
-            throw new CoreException($e);
+            );
         }
         $this->name = $name;
 
@@ -454,81 +447,60 @@ class Route
 
     protected function processKeyValidation(string $key): void
     {
-        try {
-            Validation::grouped('$key', $key)
-                ->append(
-                    'value',
-                    function (string $string): bool {
-                        return
-                            $string == '/' ?: (
-                                strlen($string) > 0
-                                && Utils\Str::startsWith('/', $string)
-                                && !Utils\Str::endsWith('/', $string)
-                                && !Utils\Str::contains('//', $string)
-                                && !Utils\Str::contains(' ', $string)
-                                && !Utils\Str::contains('\\', $string)
-                            );
-                    },
-                    "String %i must start with a forward slash, it shouldn't contain neither whitespace, backslashes or extra forward slashes and it should be specified without a trailing slash."
-                )
-                ->append(
-                    'wildcards',
-                    function (string $string): bool {
-                        return !$this->handlebars ?: preg_match_all('/{([0-9]+)}/', $string) === 0;
-                    },
-                    (string) (new Message('Wildcards in the form of %s are reserved.'))
-                        ->code('%s', '/{n}')
-                )
-                ->validate();
-        } catch (Throwable $e) {
-            throw new CoreException($e);
+        $keyValidate = $key == '/' ?: (
+            strlen($key) > 0
+            && Utils\Str::startsWith('/', $key)
+            && !Utils\Str::endsWith('/', $key)
+            && !Utils\Str::contains('//', $key)
+            && !Utils\Str::contains(' ', $key)
+            && !Utils\Str::contains('\\', $key)
+        );
+        if (!$keyValidate) {
+            throw new CoreException(
+                (new Message("String %s must start with a forward slash, it shouldn't contain neither whitespace, backslashes or extra forward slashes and it should be specified without a trailing slash."))
+                    ->code('%s', $key)
+            );
+        }
+        $wildcardValidate = !$this->handlebars ?: preg_match_all('/{([0-9]+)}/', $key) === 0;
+        if (!$wildcardValidate) {
+            throw new CoreException(
+                (new Message('Wildcards in the form of %s are reserved.'))
+                    ->code('%s', '/{n}')
+            );
         }
     }
 
     protected function processWildcardValidation(string $wildcardName, string $regex): void
     {
         $wildcard = $this->getHandlebarsWrap($wildcardName);
-        try {
-            Validation::grouped('$wildcardName', $wildcardName)
-                ->append(
-                    'value',
-                    function (string $string): bool {
-                        return
-                            !Utils\Str::startsWithNumeric($string)
-                            && preg_match('/^[a-z0-9_]+$/i', $string);
-                    },
-                    "String %s must contain only alphanumeric and underscore characters and it shouldn't start with a numeric value."
-                )
-                ->append(
-                    'match',
-                    function (string $string) use ($wildcard): bool {
-                        return
-                            Utils\Str::contains($wildcard, $this->getKey())
-                            || Utils\Str::contains('{'."$string?".'}', $this->getKey());
-                    },
-                    (string) (new Message("Wildcard %s doesn't exists in %r."))
-                        ->code('%s', $wildcard)
-                        ->code('%r', $this->getKey())
-                )
-                ->append(
-                    'unique',
-                    function (string $string): bool {
-                        return !isset($this->wheres[$string]);
-                    },
-                    (string) (new Message('Where clause for %s wildcard has been already declared.'))
-                        ->code('%s', $wildcard)
-                )
-                ->validate();
-            Validation::single(
-                '$regex',
-                $regex,
-                function (string $string): bool {
-                    return Validate::regex('/'.$string.'/');
-                },
-                'Invalid regex pattern %s.'
+        $validateFormat = !Utils\Str::startsWithNumeric($wildcardName) && preg_match('/^[a-z0-9_]+$/i', $wildcardName);
+        if (!$validateFormat) {
+            throw new CoreException(
+                (new Message("String %s must contain only alphanumeric and underscore characters and it shouldn't start with a numeric value."))
+                    ->code('%s', $wildcardName)
             );
-        } catch (Exception $e) {
-            throw new CoreException($e->getMessage());
+        }
+        $validateMatch = Utils\Str::contains($wildcard, $this->getKey()) || Utils\Str::contains('{'."$wildcardName?".'}', $this->getKey());
+        if (!$validateMatch) {
+            throw new CoreException(
+                (new Message("Wildcard %s doesn't exists in %r."))
+                    ->code('%s', $wildcard)
+                    ->code('%r', $this->getKey())
+            );
+        }
+        $validateUnique = !isset($this->wheres[$wildcardName]);
+        if (!$validateUnique) {
+            throw new CoreException(
+                (new Message('Where clause for %s wildcard has been already declared.'))
+                    ->code('%s', $wildcard)
+            );
+        }
+        $validateRegex = Validate::regex('/'.$wildcardName.'/');
+        if (!$validateRegex) {
+            throw new CoreException(
+                (new Message('Invalid regex pattern %s.'))
+                    ->code('%s', $regex)
+            );
         }
     }
 }
