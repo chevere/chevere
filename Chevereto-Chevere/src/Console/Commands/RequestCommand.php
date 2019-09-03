90643593b81e1f76f8f1ab0d56b76409171df5ac
Rodolfo Berrios <inbox@rodolfoberrios.com>
2019-09-03 16:56:08 -0400

    🚧 JSON implementation

--- Chevereto-Chevere/src/Console/Commands/RequestCommand.php
+++ Chevereto-Chevere/src/Console/Commands/RequestCommand.php
@@ -17,12 +17,17 @@ use ReflectionMethod;
 use Chevere\Http\Request;
 use Chevere\Console\Command;
 use Chevere\Contracts\App\LoaderContract;
+use Chevere\Message;
+use InvalidArgumentException;
+use JsonException;
 
 /**
  * The RequestCommand allows to pass a forged request to the App instance.
  *
  * Usage:
  * php app/console request <method> <path>
+ *
+ * Both static::COMMANDS and static::OPTIONS are intended to match Request::create parameters names.
  */
 final class RequestCommand extends Command
 {
@@ -43,18 +48,6 @@ final class RequestCommand extends Command
         ['content', 'r', Command::OPTION_OPTIONAL, 'Raw body data', null],
     ];
 
-    /** If required, this will map command arguments and options to Request::create */
-    const HTTP_REQUEST_FN_MAP = [
-        // 'uri' => 'uri',
-        // 'method' => 'method',
-        // 'parameters' => 'parameters',
-        // 'cookies' => 'cookies',
-        // 'files' => 'files',
-        // 'server' => 'server',
-        // 'content' => 'content'
-    ];
-
-
     // List of arguments which are passed as JSON
     const JSON_OPTIONS = ['parameters', 'cookies', 'files', 'server'];
 
@@ -65,26 +58,35 @@ final class RequestCommand extends Command
 
         $jsonOptions = [];
         foreach (static::JSON_OPTIONS as $v) {
-            $jsonOptions[$v] = is_string($options[$v]) ? json_decode($options[$v], true) : $options[$v];
+            if (is_string($options[$v])) {
+                try {
+                    $json = json_decode($options[$v], true, 512, JSON_THROW_ON_ERROR);
+                } catch (JsonException $e) {
+                    throw new InvalidArgumentException(
+                        (new Message('Unable to parse %o option %s as JSON (%m).'))
+                            ->code('%o', $v)
+                            ->code('%s', $options[$v])
+                            ->strtr('%m', $e->getMessage())
+                            ->toString()
+                    );
+                }
+            } else {
+                $json = $options[$v];
+            }
+            $jsonOptions[$v] = $json;
         }
 
-        $options = array_merge($options, $jsonOptions);
+        $passedArguments = array_merge($options, $jsonOptions, $arguments);
 
-        $requestArguments = [];
+        $requestFnArguments = [];
         $r = new ReflectionMethod(Request::class, 'create');
         foreach ($r->getParameters() as $requestArg) {
             $name = $requestArg->getName();
-            $requestArguments[] = $arguments[$name] ?? $requestArg->getDefaultValue() ?? null;
+            $requestFnArguments[] = $passedArguments[$name] ?? $requestArg->getDefaultValue() ?? null;
         }
-        $loader->setRequest(Request::create(...$requestArguments));
+        $loader->setRequest(Request::create(...$requestFnArguments));
         $loader->run();
 
         return 1;
     }
-
-    // private function getMappedName(string $name): string
-    // {
-    //     $mapped = self::HTTP_REQUEST_FN_MAP[$name] ?? null;
-    //     return $mapped ?? $name;
-    // }
 }
