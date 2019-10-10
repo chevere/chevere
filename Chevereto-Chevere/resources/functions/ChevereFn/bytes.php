47c78b3e1d312a6fc22fda7a4c2bc254dc42e8dc
Rodolfo Berrios <inbox@rodolfoberrios.com>
2019-10-10 16:41:36 -0300

    wip

---
+++ Chevereto-Chevere/resources/functions/ChevereFn/bytes.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevereto.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChevereFn;
+
+/**
+ * Returns bytes from size + suffix format.
+ *
+ * @param string $size bytes to be formatted, like "2 MB"
+ *
+ * @return int byte representation
+ */
+// function bytesFromString(string $size): int
+// {
+//     $suffix = strtoupper(substr($size, -2));
+//     if (strlen($suffix) == 1) {
+//         $iniToSuffix = ['M' => 'MB', 'G' => 'GB'];
+//         $suffix = $iniToSuffix[$suffix];
+//     }
+//     $value = intval($size);
+//     if (!in_array($suffix, BYTES_UNITS)) {
+//         return $value;
+//     }
+
+//     $array_search = array_search($suffix, BYTES_UNITS);
+//     if (false !== $array_search) {
+//         $powFactor = (int) $array_search + 1;
+//     } else {
+//         $powFactor = 0;
+//     }
+
+//     return (int) ($value * pow(1000, $powFactor));
+// }
+
+
+/**
+ * Get bytes from the php.ini values.
+ *
+ * Allows to get a byte representation from php.ini values which uses short format notation.
+ *
+ * @param string $size short size notation (like "2M")
+ *
+ * @return float byte representation
+ */
+// function getIni(string $size): float
+// {
+// }
--- Chevereto-Chevere/src/Bytes/Bytes.php
+++ Chevereto-Chevere/src/Bytes/Bytes.php
@@ -11,67 +11,31 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Chevere\Utility;
+namespace Chevere\Bytes;
 
-abstract class Bytes
+final class Bytes
 {
-    const UNITS = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-    /**
-     * Returns bytes from size + suffix format.
-     *
-     * @param string $size bytes to be formatted, like "2 MB"
-     * @param int    $cut  indicates the chop needed to substract the byte-unit
-     *
-     * @return int byte representation
-     */
-    public static function get(string $size, int $cut = -2): int
+    const BYTES_UNITS = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    /** @var int */
+    private $bytes;
+
+    /** @var int */
+    private $precision;
+
+    public function __construct(int $bytes)
     {
-        $suffix = strtoupper(substr($size, $cut));
-        if (strlen($suffix) == 1) {
-            $iniToSuffix = ['M' => 'MB', 'G' => 'GB'];
-            $suffix = $iniToSuffix[$suffix];
-        }
-        $value = intval($size);
-        if (!in_array($suffix, static::UNITS)) {
-            return $value;
-        }
-
-        $array_search = array_search($suffix, static::UNITS);
-        if (false !== $array_search) {
-            $powFactor = (int) $array_search + 1;
-        } else {
-            $powFactor = 0;
-        }
-
-        return (int) ($value * pow(1000, $powFactor));
+        $this->bytes = $bytes;
+        $this->precision = 0;
     }
 
-    /**
-     * Converts bytes to human readable representation.
-     *
-     * @param string $bytes bytes to be formatted
-     * @param int    $round how many decimals you want to get, default 1
-     *
-     * @return string formatted size string like 10 MB
-     */
-    public static function format($bytes, $round = 1): ?string
+    public function withPrecision(int $precision): Bytes
     {
-        if (!is_numeric($bytes)) {
-            return null;
-        }
-        if ($bytes < 1000) {
-            return "$bytes B";
-        }
-        foreach (static::UNITS as $k => $v) {
-            $multiplier = pow(1000, $k + 1);
-            $threshold = $multiplier * 1000;
-            if ($bytes < $threshold) {
-                $size = round($bytes / $multiplier, $round);
+        $new = clone $this;
+        $new->precision = $precision;
 
-                return "$size $v";
-            }
-        }
+        return $new;
     }
 
     /**
@@ -81,22 +45,31 @@ abstract class Bytes
      *
      * @return float MB representation
      */
-    public static function toMB(int $bytes): float
+    public function toMB(): float
     {
-        return round($bytes / pow(10, 6));
+        return round($this->bytes / pow(10, 6), $this->precision);
     }
 
     /**
-     * Get bytes from the php.ini values.
+     * Converts bytes to human readable representation.
      *
-     * Allows to get a byte representation from php.ini values which uses short format notation.
+     * @param int    $round how many decimals you want to get, default 1
      *
-     * @param string $size short size notation (like "2M")
-     *
-     * @return float byte representation
+     * @return string formatted size string like 10 MB
      */
-    public static function getIni(string $size): float
+    function toHumanReadable(): string
     {
-        return static::get($size, -1);
+        if ($this->bytes < 1000) {
+            return $this->bytes . ' B';
+        }
+        foreach (BYTES_UNITS as $k => $v) {
+            $multiplier = pow(1000, $k + 1);
+            $threshold = $multiplier * 1000;
+            if ($this->bytes < $threshold) {
+                $size = round($this->bytes / $multiplier, $this->precision);
+
+                return "$size $v";
+            }
+        }
     }
 }
