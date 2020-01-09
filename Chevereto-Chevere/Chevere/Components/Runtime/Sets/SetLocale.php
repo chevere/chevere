9dff31a6be3e16b91aed37a5d9744badef964063
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-01-09 17:09:37 -0300

    wip Runtime tests

--- Chevereto-Chevere/Chevere/Components/Runtime/Sets/SetLocale.php
+++ Chevereto-Chevere/Chevere/Components/Runtime/Sets/SetLocale.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Chevere\Components\Runtime\Sets;
 
-use RuntimeException;
+use Chevere\Components\Runtime\Exceptions\RuntimeException;
 use Chevere\Components\Message\Message;
 use Chevere\Components\Runtime\Traits\SetTrait;
 use Chevere\Components\Runtime\Contracts\SetContract;
@@ -22,8 +22,15 @@ class SetLocale implements SetContract
 {
     use SetTrait;
 
-    public function set(): void
+    /**
+     * Creates a new instance.
+     *
+     * @param string $value Value to pass to `setlocale` like `es_CL.UTF8`
+     * @throws RuntimeException If unable to set the locale
+     */
+    public function __construct(string $value)
     {
+        $this->value = $value;
         if (!setlocale(LC_ALL, $this->value)) {
             throw new RuntimeException(
                 (new Message('The locale functionality is not implemented on your platform, the specified locale %locale% does not exist or the category name %category% is invalid'))
--- Chevereto-Chevere/Chevere/Tests/Runtime/Sets/SetLocaleTest.php
+++ Chevereto-Chevere/Chevere/Tests/Runtime/Sets/SetLocaleTest.php
@@ -13,8 +13,24 @@ declare(strict_types=1);
 
 namespace Chevere\Tests\Runtime\Sets;
 
+use Chevere\Components\Runtime\Exceptions\RuntimeException;
+use Chevere\Components\Runtime\Sets\SetLocale;
 use PHPUnit\Framework\TestCase;
 
 final class SetLocaleTest extends TestCase
 {
+    public function testConstructInvalidArgument(): void
+    {
+        $this->expectException(RuntimeException::class);
+        new SetLocale('fakeLocale');
+    }
+
+    public function testConstruct(): void
+    {
+        foreach (['es_CL.UTF8', 'en_US.UTF8'] as $val) {
+            $set = new SetLocale($val);
+            $this->assertSame('locale', $set->name());
+            $this->assertSame($val, $set->value());
+        }
+    }
 }
