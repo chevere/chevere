406efb758d4d276b1b8f0df727ac19549b292b6a
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-01-23 21:56:39 -0300

    screening

--- Chevereto-Chevere/Chevere/Components/Screen/DebugScreen.php
+++ Chevereto-Chevere/Chevere/Components/Screen/DebugScreen.php
@@ -17,27 +17,50 @@ use Chevere\Components\Screen\Interfaces\ScreenInterface;
 use Psr\Http\Message\StreamInterface;
 
 /**
- * A completely silent screen.
+ * A debug screen.
  */
-final class SilentScreen implements ScreenInterface
+final class DebugScreen implements ScreenInterface
 {
+    private ScreenInterface $screen;
+
+    public function __construct()
+    {
+        $this->screen = new Screen;
+    }
+
     public function attach(string $display): ScreenInterface
     {
+        $this->screen
+            ->attach($this->debugWrap($display));
+
         return $this;
     }
 
     public function attachNl(string $display): ScreenInterface
     {
+        $this->screen
+            ->attachNl($this->debugWrap($display));
+
         return $this;
     }
 
     public function queue(): array
     {
-        return [];
+        return $this->screen->queue();
     }
 
     public function display(): ScreenInterface
     {
+        $this->screen->display();
+
         return $this;
+    }
+
+    private function debugWrap(string $display): string
+    {
+        $debug_backtrace = debug_backtrace(0, 1)[0];
+        xdd($debug_backtrace);
+
+        return '';
     }
 }
--- Chevereto-Chevere/Chevere/Components/Screen/Tests/ScreenTest.php
+++ Chevereto-Chevere/Chevere/Components/Screen/Tests/ScreenTest.php
@@ -13,17 +13,17 @@ declare(strict_types=1);
 
 namespace Chevere\Components\Screen\Tests;
 
-use Chevere\Components\Screen\Screen;
 use PHPUnit\Framework\TestCase;
 
 final class ScreenTest extends TestCase
 {
     public function testConstruct(): void
     {
+        $this->expectNotToPerformAssertions();
         // xdump($this);
-        screen()->debug()->attachNl('Fatal error at: ' . __FILE__)->display();
-        screen()->runtime()->attachNl(varInfo($this))->display();
-        screen()->runtime()->attachNl('Everything is OK! Keep going...')->display();
+        // screen()->debug()->attachNl('Fatal error at: ' . __FILE__)->display();
+        // screen()->runtime()->attachNl(varInfo($this))->display();
+        // screen()->runtime()->attachNl('Everything is OK! Keep going...')->display();
 
         // $screen = new Screen;
         // $screen
