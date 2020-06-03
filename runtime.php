0804b33273d087c87975d9aeadbf4976221ddec9
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-06-03 16:10:51 -0400

    //

--- runtime.php
+++ runtime.php
@@ -25,14 +25,13 @@ use Chevere\Components\Runtime\Sets\SetTimeZone;
 use Chevere\Components\Runtime\Sets\SetUriScheme;
 
 new RuntimeInstance(
-    new Runtime(
-        new SetDebug('1'),
-        // new SetErrorHandler('Chevere\Components\ExceptionHandler\ErrorHandler::error'),
-        // new SetExceptionHandler('Chevere\Components\ExceptionHandler\ExceptionHandler::exception'),
-        new SetLocale('en_US.UTF8'),
-        new SetDefaultCharset('UTF-8'),
-        new SetPrecision('16'),
-        new SetUriScheme('https'),
-        new SetTimeZone('UTC')
-    )
+    (new Runtime)
+        ->withSet(new SetDebug('1'))
+        ->withSet(new SetLocale('en_US.UTF8'))
+        ->withSet(new SetDefaultCharset('UTF-8'))
+        ->withSet(new SetPrecision('16'))
+        ->withSet(new SetUriScheme('https'))
+        ->withSet(new SetTimeZone('UTC'))
+        //->withSet(new SetErrorHandler('Chevere\Components\ExceptionHandler\ErrorHandler::error')),
+        //->withSet(new SetExceptionHandler('Chevere\Components\ExceptionHandler\ExceptionHandler::exception')),
 );
