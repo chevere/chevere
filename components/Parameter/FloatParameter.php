948f08a0db49809fa0436ab42ddcc3aec5dfd8b8
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-11-25 10:15:47 -0300

    adds float parameter

---
+++ components/Parameter/FloatParameter.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\Components\Parameter;
+
+use Chevere\Components\Parameter\Traits\ParameterTrait;
+use Chevere\Components\Type\Type;
+use Chevere\Interfaces\Parameter\FloatParameterInterface;
+use Ds\Set;
+
+final class FloatParameter implements FloatParameterInterface
+{
+    use ParameterTrait;
+
+    private float $default = 0.0;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->assertName();
+        $this->type = new Type(Type::FLOAT);
+        $this->attributes = new Set;
+    }
+
+    public function withDefault(float $default): FloatParameterInterface
+    {
+        $new = clone $this;
+        $new->default = $default;
+
+        return $new;
+    }
+
+    public function default(): float
+    {
+        return $this->default;
+    }
+}
---
+++ tests/Parameter/FloatParameterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\Tests\Parameter;
+
+use Chevere\Components\Parameter\FloatParameter;
+use PHPUnit\Framework\TestCase;
+
+final class FloatParameterTest extends TestCase
+{
+    public function testConstruct(): void
+    {
+        $parameter = new FloatParameter('name');
+        $this->assertSame(0.0, $parameter->default());
+        $default = 12.34;
+        $parameter = $parameter->withDefault($default);
+        $this->assertSame($default, $parameter->default());
+    }
+}
