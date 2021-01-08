8925fa67adac61f53fb42790e51f9463c2939e1a
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-01-08 09:34:50 -0300

    typo

--- src/Chevere/Interfaces/Workflow/StepInterface.php
+++ src/Chevere/Interfaces/Workflow/StepInterface.php
@@ -26,10 +26,10 @@ interface StepInterface
     public function __construct(string $action);
 
     /**
-     * Return an instance with the specified named `$namedArguments`.
+     * Return an instance with the specified `$namedArguments`.
      *
      * This method MUST retain the state of the current instance, and return
-     * an instance that contains the specified named `$namedArguments`.
+     * an instance that contains the specified `$namedArguments`.
      */
     public function withArguments(mixed ...$namedArguments): self;
 
