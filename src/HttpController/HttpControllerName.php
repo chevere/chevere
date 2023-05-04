38ea6ae79821995f1e14b9c22b5e0c565690c80a
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2023-05-04 17:11:27 -0400

    http controller name

---
+++ src/HttpController/HttpControllerName.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\Controller;
+
+use Chevere\Controller\Interfaces\ControllerNameInterface;
+use Chevere\HttpController\Interfaces\HttpControllerInterface;
+use Chevere\HttpController\Interfaces\HttpControllerNameInterface;
+
+final class HttpControllerName implements HttpControllerNameInterface
+{
+    private ControllerNameInterface $controllerName;
+
+    public function __construct(string $name)
+    {
+        $this->controllerName = new ControllerName($name);
+        $this->controllerName->assertInterface(HttpControllerInterface::class);
+    }
+
+    public function __toString(): string
+    {
+        return $this->controllerName->__toString();
+    }
+}
