90442a13ef14c7df19d6cd11fc71a8766f030ced
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2023-08-16 19:19:21 -0400

    add vectored interface

---
+++ src/DataStructure/Interfaces/VectoredInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\DataStructure\Interfaces;
+
+use Countable;
+use Iterator;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Describes the component in charge of defining a mapped interface.
+ *
+ * @template-covariant TValue
+ * @extends IteratorAggregate<TValue>
+ */
+interface VectoredInterface extends Countable, IteratorAggregate
+{
+    /**
+     * @return Traversable<TValue>
+     * @phpstan-return Iterator<TValue>
+     */
+    public function getIterator(): Iterator;
+}
