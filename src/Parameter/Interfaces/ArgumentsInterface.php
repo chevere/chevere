92baa9639ab6f45462c543203beff58a6dc74dc2
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2023-10-01 18:25:38 -0300

    random selling

--- src/Parameter/Interfaces/ArgumentsInterface.php
+++ src/Parameter/Interfaces/ArgumentsInterface.php
@@ -14,8 +14,6 @@ declare(strict_types=1);
 namespace Chevere\Parameter\Interfaces;
 
 use Chevere\Common\Interfaces\ToArrayInterface;
-use Chevere\Throwable\Exceptions\InvalidArgumentException;
-use Chevere\Throwable\Exceptions\OutOfBoundsException;
 
 /**
  * Describes the component in charge of defining a set of parameters with arguments.
@@ -39,9 +37,6 @@ interface ArgumentsInterface extends ParametersAccessInterface, ToArrayInterface
      *
      * This method MUST retain the state of the current instance, and return
      * an instance that contains the specified argument.
-     *
-     * @throws InvalidArgumentException
-     * @throws OutOfBoundsException If `$name` is not a known parameter.
      */
     public function withPut(string $name, mixed $value): self;
 
@@ -53,21 +48,16 @@ interface ArgumentsInterface extends ParametersAccessInterface, ToArrayInterface
     /**
      * Provides access to the argument value for the parameter `$name`.
      * @return mixed Value OR null (when `$name` is an optional parameter with no value)
-     * @throws OutOfBoundsException
      */
     public function get(string $name): mixed;
 
     /**
      * Provides access to the required argument for the parameter `$name`.
-     *
-     * @throws OutOfBoundsException
      */
     public function required(string $name): CastArgumentInterface;
 
     /**
      * Provides access to the optional argument for the parameter `$name`.
-     *
-     * @throws OutOfBoundsException
      */
     public function optional(string $name): ?CastArgumentInterface;
 }
